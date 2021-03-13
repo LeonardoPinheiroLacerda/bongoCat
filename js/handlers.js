33b7a5646d2d0c64c9f663dd226dcb0bfc33646e
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2021-03-13 03:26:56 -0300

    bongos and meow added

---
+++ js/handlers.js
@@ -0,0 +1,63 @@
+var rightPaw = document.getElementsByClassName('rightPaw')[0];
+var leftPaw = document.getElementsByClassName('leftPaw')[0];
+var mouth = document.getElementsByClassName('mouth')[0];
+
+var bongo0 = false;
+var bongo1 = false;
+var meow = false;
+
+document.onkeypress = function(e){
+    switch(e.key){
+        case 'a':
+        case 'A':    
+            rightPaw.id = "rightDown";
+            if(!bongo0){
+                var audio = new Audio('sounds/bongo0.mp3');
+                audio.play();
+                bongo0 =true;
+            }
+            break;
+
+        case 'd':
+        case 'D':
+            leftPaw.id = "leftDown";
+            if(!bongo1){
+                var audio = new Audio('sounds/bongo1.mp3');
+                audio.play();
+                bongo1 =true;
+            }
+            break;
+
+        case ' ':
+            mouth.id = "openned";
+            if(!meow){
+                var audio = new Audio('sounds/meow.mp3');
+                audio.play();
+                meow = true;
+            }
+            break;
+
+    }
+}
+
+
+document.onkeyup = function(e){
+    switch(e.key){
+        case 'a':
+        case 'A':    
+            rightPaw.id = "rightUp";
+            bongo0 = false;
+            break;
+
+        case 'd':
+        case 'D':
+            leftPaw.id = "leftUp";
+            bongo1 = false;
+            break;
+        
+        case ' ':
+            mouth.id = "closed";
+            meow = false;
+            break;
+    }
+}
