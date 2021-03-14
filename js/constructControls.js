6d27dbff1cdbb68a292a5b493dbbaa1c47c894bf
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2021-03-14 03:12:49 -0300

    architecture correction

--- js/constructControls.js
+++ js/constructControls.js
@@ -1,88 +1,122 @@
 var controls = document.getElementsByClassName('controls')[0];
 
-function bongoBottons(){
+var typeButtons = {
+    big : "big",
+    medium : "medium",
+    small : "small"
+}
 
+var animations = {
+    bigButtonClick : "bigButtonClick",
+    bigButtonRelease : "bigButtonRelease",
+
+    mediumButtonClick : "mediumButtonClick",
+    mediumButtonRelease : "mediumButtonRelease",
+
+    smallButtonClick : "smallButtonClick",
+    smallButtonRelease : "smallButtonRelease"
+}
+
+function doAnimation(obj, animation){
+    obj.style.animation = animation;
+    obj.style.animationDuration = '0.1s';
+
+    var flag;
+    var typeButton;
+
+    switch(animation){
+        case animations.bigButtonClick:
+            typeButton = typeButtons.big;
+            flag = true;
+            break;
+
+        case animations.bigButtonRelease:
+            typeButton = typeButtons.big;
+            flag = false;
+            break;
+
+        case animations.mediumButtonClick:
+            typeButton = typeButtons.medium;
+            flag = true;
+            break;
+
+        case animations.mediumButtonRelease:
+            typeButton = typeButtons.medium;
+            flag = false;
+            break;
+
+        case animations.smallButtonClick:
+            typeButton = typeButtons.small;
+            flag = true;
+            break;
+
+        case animations.smallButtonRelease:
+            typeButton = typeButtons.small;
+            flag = false;
+            break;
+    }
+    
+
+    if(flag){
+        obj.classList.add(typeButton + "ButtonClicked");
+    }else{
+        obj.classList.remove(typeButton + "ButtonClicked");
+    }
+}
+
+function constructButton(typeButton, action, text){
+    var obj = document.createElement('div');
+    
+    obj.classList.add(typeButton + "Button");
+    obj.classList.add("control");
+
+    obj.onpointerdown = function(){
+        action.do();
+        switch(typeButton){
+            case typeButtons.big:
+                doAnimation(obj, animations.bigButtonClick);
+                break;
+            case typeButtons.medium:
+                doAnimation(obj, animations.mediumButtonClick);
+                break;
+            case typeButtons.small:
+                doAnimation(obj, animations.smallButtonClick);
+                break;
+        }
+    }
+
+    obj.onpointerup = function(){
+        action.undo();
+        switch(typeButton){
+            case typeButtons.big:
+                doAnimation(obj, animations.bigButtonRelease);
+                break;
+            case typeButtons.medium:
+                doAnimation(obj, animations.mediumButtonRelease);
+                break;
+            case typeButtons.small:
+                doAnimation(obj, animations.smallButtonRelease);
+                break;
+        }
+    }
+
+    obj.onselectstart = function(){return false}
+    obj.oncontextmenu = function(){return false}
+    
+    var span = document.createElement('span');
+    span.innerHTML = text;
+
+    obj.appendChild(span);
+    controls.appendChild(obj);
 }
 
 function meowButton(){
-    var meow = document.createElement('div');
-    
-    meow.classList.add("bigButton");
-    meow.classList.add("control");
-
-    meow.onpointerdown = function(){
-        mouthAction.do();
-
-        meow.style.animation = "bigButtonClick";
-        meow.style.animationDuration = '0.1s';
-
-        meow.classList.add("bigButtonClicked");
-    }
-
-    meow.onpointerup = function(){
-        mouthAction.undo();
-
-        meow.style.animation = "bigButtonRelease";
-        meow.style.animationDuration = '0.1s';
-
-        meow.classList.remove("bigButtonClicked");
-    }
-
-    meow.onselectstart = function(){return false}
-    meow.oncontextmenu = function(){return false}
-    
-    var span = document.createElement('span');
-    span.innerHTML = 'Meow';
-
-    meow.appendChild(span);
-    controls.appendChild(meow);
-    
+    constructButton(typeButtons.big, mouthAction, "Meow");    
 }
 
 function bongoButtons(){
-    var right = document.createElement('div');
-    var left = document.createElement('div');
-
-    right.classList.add('mediumButton');
-    left.classList.add('mediumButton');
-
-    right.classList.add("control");
-    left.classList.add("control");
-
-    left.onpointerdown = function(){
-        bongo0Action.do();
-    }
-
-    left.onpointerup = function(){
-        bongo0Action.undo();
-    }
-
-    right.onpointerdown = function(){
-        bongo1Action.do();
-    }
-
-    right.onpointerup = function(){
-        bongo1Action.undo();
-    }
-
-    right.onselectstart = function(){return false}
-    right.oncontextmenu = function(){return false}
-
-    left.onselectstart = function(){return false}
-    left.oncontextmenu = function(){return false}
-
-    var rightSpan = document.createElement('span');
-    var leftSpan = document.createElement('span');
-
-    rightSpan.innerHTML = "Right";
-    leftSpan.innerHTML = "Left";
-
-    left.appendChild(leftSpan);
-    right.appendChild(rightSpan);
-   
-    controls.appendChild(left);
-    controls.appendChild(right);
-    
+    constructButton(typeButtons.medium, bongo0Action, "Left");    
+    constructButton(typeButtons.medium, bongo1Action, "Right");
 }
 
 
